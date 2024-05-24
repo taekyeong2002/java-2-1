9cf3599320f20a0c74a9460ebad7b18fe138a842
taekyeong2002 <taekyeong2002@naver.com>
2024-05-24 11:24:02 +0900

    commit

---
+++ src/Ea2.5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ea_2.5 {
+
+     public static void main(String.args){
+        Scanner scanner = new Scanner(System.ln);
+
+    System.out.println();
+
+
+
+}
---
+++ src/Ea3_4.java
@@ -0,0 +1,12 @@
+public class Ea3_4 {
+    public static void main (String[] args){
+
+        for(int i=1; i<10; i++) {
+            for(int j=1; j<10; j++){
+                System.out.print(i + "x" + j + "=" + i+j);
+                ;
+            }
+            System.out.println();
+        }
+    }    
+}
